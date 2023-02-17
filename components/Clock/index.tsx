f1768322608dd7be5d34fc18df72a40a26a8de24
mehedy hassan <mehedy@algosolver.com>
2023-02-18 03:45:07 +0600

    landing page design made

--- components/Clock/index.tsx
+++ components/Clock/index.tsx
@@ -19,31 +19,31 @@ console.log({date})
   };
 console.log("hour, minute, seconds", date?.getHours(), date?.getMinutes(), date?.getSeconds())
   return (
-    <div className="clock">
+    <div className={styles.clock}>
         <div
-          className="hour_hand"
+          className={styles.hour_hand}
           style={hourHandStyle}
         />
         <div
-          className="min_hand"
+          className={styles.min_hand}
           style={minuteHandStyle}
         />
         <div
-          className="sec_hand"
+          className={styles.sec_hand}
           style={secondHandStyle}
         />
-        <span className="twelve">12</span>
-        <span className="one">1</span>
-        <span className="two">2</span>
-        <span className="three">3</span>
-        <span className="four">4</span>
-        <span className="five">5</span>
-        <span className="six">6</span>
-        <span className="seven">7</span>
-        <span className="eight">8</span>
-        <span className="nine">9</span>
-        <span className="ten">10</span>
-        <span className="eleven">11</span>
+        <span className={styles.twelve}>|</span>
+        <span className={styles.one}>|</span>
+        <span className={styles.two}>|</span>
+        <span className={styles.three}>|</span>
+        <span className={styles.four}>|</span>
+        <span className={styles.five}>|</span>
+        <span className={styles.six}>|</span>
+        <span className={styles.seven}>|</span>
+        <span className={styles.eight}>|</span>
+        <span className={styles.nine}>|</span>
+        <span className={styles.ten}>|</span>
+        <span className={styles.eleven}>|</span>
       </div>
     
     // <div className="flex justify-center py-10 group">
--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -3,10 +3,13 @@ import Clock from "../Clock";
 const HomePage = () => {
   return (
     <div className="bg-home-banner-image bg-cover h-screen relative">
-      {/* <div className="absolute flex flex-col leading-none justify-center top-0 left-0 right-0 bottom-0 m-auto rounded-md  py-2 text-[50px] font-semibold text-white text-center h-[10vh] w-[300px] animate-pulse">
+      <div className="absolute flex flex-col leading-10 justify-center  rounded-md  py-2 text-[20px] font-semibold text-white text-center h-[10vh] w-[300px]">
+        <span>fourbit</span>
+      </div>
+      <div className="absolute flex flex-col leading-none justify-center top-0 left-0 right-0 bottom-0 m-auto rounded-md  py-2 text-[50px] font-semibold text-white text-center w-full h-full">
        <Clock/>
-      </div> */}
-      <div className="absolute flex flex-col leading-none justify-center top-0 left-0 right-0 bottom-0 m-auto rounded-md  py-2 text-[50px] font-semibold text-white text-center h-[10vh] w-[300px] animate-pulse">
+      </div>
+      <div className="absolute flex gap-4 justify-center left-0 right-0 bottom-16 m-auto rounded-md  py-2 text-[40px] font-semibold text-white text-center h-[10vh] w-[300px] animate-pulse">
         <span>COMING</span>
         <span>SOON</span>
       </div>
