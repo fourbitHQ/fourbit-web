45c810d9597f1caea4aa1cd367832b7ce1476901
mehedy hassan <mehedy@algosolver.com>
2023-02-18 23:34:36 +0600

    clock position refactored

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -6,10 +6,10 @@ const HomePage = () => {
       <div className="absolute flex flex-col leading-10 justify-center  rounded-md  py-2 text-[20px] font-semibold text-white text-center h-[10vh] w-[300px]">
         <span>fourbit</span>
       </div>
-      <div className="absolute flex flex-col leading-none justify-center top-0 left-0 right-0 bottom-0 m-auto rounded-md  py-2 text-[50px] font-semibold text-white text-center w-full h-full">
+      <div className="absolute flex flex-col leading-none rounded-md  py-2 text-[50px] font-semibold text-white text-center w-full h-full">
        <Clock/>
       </div>
-      <div className="absolute flex gap-4 justify-center left-0 right-0 bottom-16 m-auto rounded-md  py-2 text-[40px] font-semibold text-white text-center h-[10vh] w-[300px] animate-pulse">
+      <div className="absolute flex gap-4 justify-center left-0 right-0 bottom-40 m-auto rounded-md  py-2 text-[40px] font-semibold text-white text-center h-[10vh] w-[300px] animate-pulse">
         <span>COMING</span>
         <span>SOON</span>
       </div>
